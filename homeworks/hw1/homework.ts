ab3bb68a32da4286a66ae4c1a35bc74293dd4087
Maxim Dvoynishnikov <maxim.dvoynishnikov@tccenter.ru>
2020-03-04 15:29:13 +0300

    hw1 done commit

--- homeworks/hw1/homework.ts
+++ homeworks/hw1/homework.ts
@@ -130,154 +130,146 @@ function parse(
     return useRounding ? round(v) : v;
 }
 
-currency.prototype = {
-    /**
-     * Adds values together.
-     * @param {number} number
-     * @returns {currency}
-     */
-    add(number) {
-        let { intValue, _settings, _precision } = this;
-        return currency(
-            (intValue += parse(number, _settings)) / _precision,
-            _settings,
+/**
+ * Adds values together.
+ * @param {number} number
+ * @returns {currency}
+ */
+currency.prototype.add = function(number) {
+    let { intValue, _settings, _precision } = this;
+    return currency(
+        (intValue += parse(number, _settings)) / _precision,
+        _settings,
+    );
+};
+
+/**
+ * Subtracts value.
+ * @param {number} number
+ * @returns {currency}
+ */
+currency.prototype.subtract = function(number) {
+    let { intValue, _settings, _precision } = this;
+    return currency(
+        (intValue -= parse(number, _settings)) / _precision,
+        _settings,
+    );
+};
+
+/**
+ * Multiplies values.
+ * @param {number} number
+ * @returns {currency}
+ */
+currency.prototype.multiply = function(number) {
+    let { intValue, _settings } = this;
+    return currency((intValue *= number) / pow(_settings.precision), _settings);
+};
+
+/**
+ * Divides value.
+ * @param {number} number
+ * @returns {currency}
+ */
+currency.prototype.divide = function(number) {
+    let { intValue, _settings } = this;
+    return currency((intValue /= parse(number, _settings, false)), _settings);
+};
+
+/**
+ * Takes the currency amount and distributes the values evenly. Any extra pennies
+ * left over from the distribution will be stacked onto the first set of entries.
+ * @param {number} count
+ * @returns {array}
+ */
+currency.prototype.distribute = function(count) {
+    let { intValue, _precision, _settings } = this,
+        distribution: Value[] = [],
+        split = Math[intValue >= 0 ? 'floor' : 'ceil'](intValue / count),
+        pennies = Math.abs(intValue - split * count);
+
+    for (; count !== 0; count--) {
+        let item = currency(split / _precision, _settings);
+
+        // Add any left over pennies
+        pennies-- > 0 &&
+            (item =
+                intValue >= 0
+                    ? item.add(1 / _precision)
+                    : item.subtract(1 / _precision));
+
+        distribution.push(item);
+    }
+
+    return distribution;
+};
+
+/**
+ * Returns the dollar value.
+ * @returns {number}
+ */
+currency.prototype.dollars = function() {
+    return ~~this.value;
+};
+
+/**
+ * Returns the cent value.
+ * @returns {number}
+ */
+currency.prototype.cents = function() {
+    let { intValue, _precision } = this;
+    return ~~(intValue % _precision);
+};
+
+/**
+ * Formats the value as a string according to the formatting settings.
+ * @param {boolean} useSymbol - format with currency symbol
+ * @returns {string}
+ */
+currency.prototype.format = function(useSymbol) {
+    let {
+            pattern,
+            negativePattern,
+            formatWithSymbol,
+            symbol,
+            separator,
+            decimal,
+            groups,
+        } = this._settings,
+        values = (this + '').replace(/^-/, '').split('.'),
+        dollars = values[0],
+        cents = values[1];
+
+    // set symbol formatting
+    typeof useSymbol === 'undefined' && (useSymbol = formatWithSymbol);
+
+    return (this.value >= 0 ? pattern : negativePattern)
+        .replace('!', useSymbol ? symbol : '')
+        .replace(
+            '#',
+            `${dollars.replace(groups, '$1' + separator)}${
+                cents ? decimal + cents : ''
+            }`,
         );
-    },
+};
 
-    /**
-     * Subtracts value.
-     * @param {number} number
-     * @returns {currency}
-     */
-    subtract(number) {
-        let { intValue, _settings, _precision } = this;
-        return currency(
-            (intValue -= parse(number, _settings)) / _precision,
-            _settings,
-        );
-    },
+/**
+ * Formats the value as a string according to the formatting settings.
+ * @returns {string}
+ */
+currency.prototype.toString = function() {
+    let { intValue, _precision, _settings } = this;
+    return rounding(intValue / _precision, _settings.increment).toFixed(
+        _settings.precision,
+    );
+};
 
-    /**
-     * Multiplies values.
-     * @param {number} number
-     * @returns {currency}
-     */
-    multiply(number) {
-        let { intValue, _settings } = this;
-        return currency(
-            (intValue *= number) / pow(_settings.precision),
-            _settings,
-        );
-    },
-
-    /**
-     * Divides value.
-     * @param {number} number
-     * @returns {currency}
-     */
-    divide(number) {
-        let { intValue, _settings } = this;
-        return currency(
-            (intValue /= parse(number, _settings, false)),
-            _settings,
-        );
-    },
-
-    /**
-     * Takes the currency amount and distributes the values evenly. Any extra pennies
-     * left over from the distribution will be stacked onto the first set of entries.
-     * @param {number} count
-     * @returns {array}
-     */
-    distribute(count) {
-        let { intValue, _precision, _settings } = this,
-            distribution: Value[] = [],
-            split = Math[intValue >= 0 ? 'floor' : 'ceil'](intValue / count),
-            pennies = Math.abs(intValue - split * count);
-
-        for (; count !== 0; count--) {
-            let item = currency(split / _precision, _settings);
-
-            // Add any left over pennies
-            pennies-- > 0 &&
-                (item =
-                    intValue >= 0
-                        ? item.add(1 / _precision)
-                        : item.subtract(1 / _precision));
-
-            distribution.push(item);
-        }
-
-        return distribution;
-    },
-
-    /**
-     * Returns the dollar value.
-     * @returns {number}
-     */
-    dollars() {
-        return ~~this.value;
-    },
-
-    /**
-     * Returns the cent value.
-     * @returns {number}
-     */
-    cents() {
-        let { intValue, _precision } = this;
-        return ~~(intValue % _precision);
-    },
-
-    /**
-     * Formats the value as a string according to the formatting settings.
-     * @param {boolean} useSymbol - format with currency symbol
-     * @returns {string}
-     */
-    format(useSymbol) {
-        let {
-                pattern,
-                negativePattern,
-                formatWithSymbol,
-                symbol,
-                separator,
-                decimal,
-                groups,
-            } = this._settings,
-            values = (this + '').replace(/^-/, '').split('.'),
-            dollars = values[0],
-            cents = values[1];
-
-        // set symbol formatting
-        typeof useSymbol === 'undefined' && (useSymbol = formatWithSymbol);
-
-        return (this.value >= 0 ? pattern : negativePattern)
-            .replace('!', useSymbol ? symbol : '')
-            .replace(
-                '#',
-                `${dollars.replace(groups, '$1' + separator)}${
-                    cents ? decimal + cents : ''
-                }`,
-            );
-    },
-
-    /**
-     * Formats the value as a string according to the formatting settings.
-     * @returns {string}
-     */
-    toString() {
-        let { intValue, _precision, _settings } = this;
-        return rounding(intValue / _precision, _settings.increment).toFixed(
-            _settings.precision,
-        );
-    },
-
-    /**
-     * Value for JSON serialization.
-     * @returns {float}
-     */
-    toJSON() {
-        return this.value;
-    },
+/**
+ * Value for JSON serialization.
+ * @returns {float}
+ */
+currency.prototype.toJSON = function() {
+    return this.value;
 };
 
 export default currency;
